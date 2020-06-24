b7385c30ab4f7d695da1f0d56d334dbb08baab0b
Баранцев Степан <stepan.barantsev@gmail.com>
2020-06-25 02:47:57 +0300

    Первый тест с проверками

--- src/test/java/ru/task/firstTest.kt
+++ src/test/java/ru/task/firstTest.kt
@@ -1,9 +1,12 @@
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class HelloJunit5Test {
 
     @Test
-    fun `First test`() {
-        print("Hello, JUnit5!")
+    fun `Base assertions`() {
+        assertEquals("a", "a")
+        assertEquals(2, 1 + 1, "Optional message")
+        assertEquals(2, 1 + 1, { "Assertion message " + "can be lazily evaluated" })
     }
 }
