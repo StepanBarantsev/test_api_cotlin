4e60cccb765ce9764b341505f4b0607c88c71df7
Баранцев Степан <stepan.barantsev@gmail.com>
2020-06-27 23:14:44 +0300

    Отправка первого пробного запроса

--- src/test/java/ru/task/Base.kt
+++ src/test/java/ru/task/Base.kt
@@ -1,5 +1,11 @@
 package ru.task
 
+import io.restassured.RestAssured.*
+
 open class BaseTest {
-    val baseUrl = "aaa"
+    val baseUrl = "https://api.worldoftanks.ru/wot/"
+
+    fun getResponse(pathRequest: String) : String{
+        return get("$baseUrl$pathRequest").getBody().asString()
+    }
 }
