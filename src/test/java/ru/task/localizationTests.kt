5732ecf02d56d7f311fd82c987af7f047aefce1d
Баранцев Степан <stepan.barantsev@gmail.com>
2020-06-27 21:51:59 +0300

    Переименовал методы

--- src/test/java/ru/task/localizationTests.kt
+++ src/test/java/ru/task/localizationTests.kt
@@ -3,7 +3,7 @@ package ru.task
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-class SearchTypeTest {
+class TestsLocalization {
 
     @Test
     fun testFirst() {
--- src/test/java/ru/task/searchtypeTests.kt
+++ src/test/java/ru/task/searchtypeTests.kt
@@ -3,7 +3,7 @@ package ru.task
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-class LimitTest {
+class TestsSearchType {
 
     @Test
     fun testFirst() {
