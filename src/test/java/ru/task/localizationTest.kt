46904375dc7e94acf7cc00b8216d586688eb468a
Баранцев Степан <stepan.barantsev@gmail.com>
2020-06-27 21:33:08 +0300

    Изменил названия методов

--- src/test/java/ru/task/localizationTest.kt
+++ src/test/java/ru/task/localizationTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class LocalizationTest {
 
     @Test
-    fun `Base assertions`() {
+    fun testFirst() {
         assertEquals("a", "a")
         assertEquals(2, 1 + 1, "Optional message")
         assertEquals(2, 1 + 1, { "Assertion message " + "can be lazily evaluated" })
