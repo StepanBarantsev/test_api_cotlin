db83964bd2c09d8b77bf6924489dba61e4bdbdbe
Баранцев Степан <stepan.barantsev@gmail.com>
2020-06-27 21:24:23 +0300

    Меняю названия обратно

--- src/test/java/ru/task/localizationTest.kt
+++ src/test/java/ru/task/localizationTest.kt
@@ -3,7 +3,7 @@ package ru.task
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-class LocalizationTests {
+class LocalizationTest {
 
     @Test
     fun `Base assertions`() {
