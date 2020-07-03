27bbc1ab893a1eff0c48713641e3d31a179e3d04
barantsevindriver <67633193+barantsevindriver@users.noreply.github.com>
2020-07-03 10:10:09 +0300

    Описания для лимитов

--- src/test/java/ru/task/tests/Accounts/Limit/limitTests.kt
+++ src/test/java/ru/task/tests/Accounts/Limit/limitTests.kt
@@ -25,7 +25,7 @@ class TestsLimit {
 
     @Test
     @DisplayName("Отправка запроса с валидным параметром limit (от 1 до 100)")
-    @Description("Отправка запроса с валидным параметром limit (от 1 до 100)")
+    @Description("Отправка запроса с валидным параметром limit (от 1 до 100). Ожидается, что data.size == meta[count] == limit.")
     fun testRandomValidLimit() {
         val randNum = Random.nextInt(1,app.accountsHelper.defaultLimit)
         val response = app.accountsHelper.sendAccountsRequest(search=partOfName, limit=randNum)
@@ -37,7 +37,7 @@ class TestsLimit {
 
     @ParameterizedTest(name="Отправка запроса с валидным граничным значением {0}")
     @ValueSource(ints = [1, 100])
-    @Description("Отправка запроса с валиднми граничными значениями параметра limit (1 и 100)")
+    @Description("Отправка запроса с валиднми граничными значениями параметра limit (1 и 100). Ожидается, что data.size == meta[count] == limit.")
     fun testValidBorderLimits(num: Int) {
         val response = app.accountsHelper.sendAccountsRequest(search=partOfName, limit=num)
 
@@ -49,7 +49,7 @@ class TestsLimit {
     // При вводе любого некорректного числового лимита (больше/меньше) возвращается default
     @ParameterizedTest(name="Отправка запроса с невалидным граничным значением {0}")
     @ValueSource(ints = [0, 101])
-    @Description("Отправка запроса с невалидными граничными значениями параметра limit (0 и 101)")
+    @Description("Отправка запроса с невалидными граничными значениями параметра limit (0 и 101). Ожидается, что data.size == meta[count] == 100.")
     fun testInvalidBorderLimits(num: Int) {
         val response = app.accountsHelper.sendAccountsRequest(search=partOfName, limit=num)
 
@@ -59,7 +59,7 @@ class TestsLimit {
 
     @Test
     @DisplayName("Отправка запроса с значениями параметра Limit, превышающими максимально возможный")
-    @Description("Отправка запроса с значениями параметра Limit, превышающими максимально возможный")
+    @Description("Отправка запроса с значениями параметра Limit, превышающими максимально возможный. Ожидается, что data.size == meta[count] == 100.")
     fun testRandomNumberMoreThenLimit() {
         val randNum = Random.nextInt(app.accountsHelper.defaultLimit + 1,app.accountsHelper.defaultLimit * 10)
         val response = app.accountsHelper.sendAccountsRequest(search=partOfName, limit=randNum)
@@ -70,7 +70,7 @@ class TestsLimit {
 
     @Test
     @DisplayName("Отправка запроса без указания параметра limit")
-    @Description("Отправка запроса без указания параметра limit")
+    @Description("Отправка запроса без указания параметра limit. Ожидается, что data.size == meta[count] == 100.")
     fun testWithoutLimit() {
         val response = app.accountsHelper.sendAccountsRequest(search=partOfName)
 
@@ -80,7 +80,7 @@ class TestsLimit {
 
     @Test
     @DisplayName("Отправка запроса с пустым параметром limit")
-    @Description("Отправка запроса с пустым параметром limit")
+    @Description("Отправка запроса с пустым параметром limit. Ожидается, что data.size == meta[count] == 100.")
     fun testEmptyLimit() {
         val response = app.accountsHelper.sendAccountsRequest(search=partOfName, limit="")
 
@@ -90,7 +90,7 @@ class TestsLimit {
 
     @Test
     @DisplayName("Отправка запроса с отрицательным параметром limit")
-    @Description("Отправка запроса с отрицательным параметром limit")
+    @Description("Отправка запроса с отрицательным параметром limit. Ожидается, что data.size == meta[count] == 100.")
     fun testNegativeLimit() {
         val randNum = Random.nextInt(-200,-1)
         val response = app.accountsHelper.sendAccountsRequest(search=partOfName, limit=randNum)
@@ -101,7 +101,7 @@ class TestsLimit {
 
     @Test
     @DisplayName("Отправка запроса с нечисловым параметром limit")
-    @Description("Отправка запроса с нечисловым параметром limit")
+    @Description("Отправка запроса с нечисловым параметром limit. Ожидается ошибка 407 (INVALID_LIMIT)")
     fun testStingLimit() {
         val str = "fdkdfkdfksdksdksdj"
         val response: Accounts = app.accountsHelper.sendAccountsRequest(search=partOfName, limit=str)
